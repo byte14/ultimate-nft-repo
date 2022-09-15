6d150dce0795ff471b6986b8d4992ee26539f187
byte14 <avishekrajpanta@gmail.com>
2022-09-15 14:02:57 +0545

    fixed typo

--- hardhat.config.js
+++ hardhat.config.js
@@ -17,7 +17,7 @@ module.exports = {
     },
     goerli: {
       chainId: 5,
-      blockConfirmtions: 6,
+      blockConfirmations: 6,
       url: process.env.GOERLI_RPC_URL,
       accounts: [process.env.PRIVATE_KEY],
     },
