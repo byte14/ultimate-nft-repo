aa891e89b195b2585861d5bbe719d71ea81dcb28
byte14 <avishekrajpanta@gmail.com>
2022-09-21 00:12:15 +0545

    add & update functions

--- contracts/OnChainSvgNFT.sol
+++ contracts/OnChainSvgNFT.sol
@@ -4,44 +4,104 @@ pragma solidity ^0.8.12;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/utils/Base64.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+error TokenNotExisted();
 
 contract OnChainSvgNFT is ERC721 {
     using Counters for Counters.Counter;
 
+    struct TokenValue {
+        uint256 bullValue;
+        uint256 bearValue;
+    }
+
+    AggregatorV3Interface private immutable i_priceFeed;
     Counters.Counter private s_tokenCounter;
-    string private s_bullImageURI;
-    string private s_bearimageURI;
-    string private s_neutralImageURI;
+    string[] private s_imagesURIs;
+
+    mapping(uint256 => TokenValue) public s_tokenValues;
 
     event NftMinted(uint256 indexed tokenId, address minter);
 
-    constructor(
-        string memory bullSvg,
-        string memory bearSvg,
-        string memory neutralSvg
-    ) ERC721("On Chain SVG", "OCN") {
-        s_bullImageURI = svgToImageURI(bullSvg);
-        s_bearimageURI = svgToImageURI(bearSvg);
-        s_neutralImageURI = svgToImageURI(neutralSvg);
+    constructor(address priceFeedAddress, string[3] memory svgs)
+        ERC721("Bull Bear Neutral", "BBN")
+    {
+        i_priceFeed = AggregatorV3Interface(priceFeedAddress);
+        svgToImageURI(svgs);
     }
 
-    function mintNFT() external {
+    function mintNFT(uint256 bullValue, uint256 bearValue) external {
         uint256 newTokenId = s_tokenCounter.current();
+        s_tokenValues[newTokenId] = TokenValue(bullValue, bearValue);
         _safeMint(msg.sender, newTokenId);
         s_tokenCounter.increment();
         emit NftMinted(newTokenId, msg.sender);
     }
 
-    function svgToImageURI(string memory svg)
-        private
-        pure
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override
         returns (string memory)
     {
-        bytes memory imageURI = abi.encodePacked(svg);
-        return
-            string.concat(
+        if (!_exists(tokenId)) {
+            revert TokenNotExisted();
+        }
+
+        (, int256 _price, , , ) = i_priceFeed.latestRoundData();
+        uint256 price = uint256(_price * 1e10);
+        TokenValue memory token = s_tokenValues[tokenId];
+        string memory name;
+        string memory description;
+        string memory imageURI;
+        string[] memory imagesURIs = s_imagesURIs;
+
+        if (price >= token.bullValue) {
+            name = "Bull";
+            description = "Market is bullish";
+            imageURI = imagesURIs[0];
+        } else if (price <= token.bearValue) {
+            name = "Bear";
+            description = "Market is bearish";
+            imageURI = imagesURIs[1];
+        } else if (price > token.bearValue && price < token.bullValue) {
+            name = "Neutral";
+            description = "Market is neutral";
+            imageURI = imagesURIs[2];
+        }
+
+        bytes memory dataURI = abi.encodePacked(
+            '{"name":"',
+            name,
+            '", "description":"',
+            description,
+            '", "image":"',
+            imageURI,
+            '"}'
+        );
+        return string.concat(_baseURI(), Base64.encode(dataURI));
+    }
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "data:application/json;base64,";
+    }
+
+    function svgToImageURI(string[3] memory svgs) private {
+        for (uint256 i = 0; i < svgs.length; i++) {
+            string memory imageURI = string.concat(
                 "data:image/svg+xml;base64,",
-                Base64.encode(imageURI)
+                Base64.encode(abi.encodePacked(svgs[i]))
             );
+            s_imagesURIs.push(imageURI);
+        }
+    }
+
+    function getTokenCounter() external view returns (uint256) {
+        return s_tokenCounter.current();
+    }
+
+    function getImageURI(uint256 index) external view returns (string memory) {
+        return s_imagesURIs[index];
     }
 }
